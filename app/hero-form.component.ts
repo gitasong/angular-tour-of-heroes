684593f2738226a0d91a55683e54cfe84fb58d46
Nicole Freed <nicole.freed@gmail.com>
2017-08-15 17:21:39 -0700

    create HeroFormComponent

---
+++ app/hero-form.component.ts
@@ -0,0 +1,25 @@
+// let myHero = new Hero(42, 'SkyDog', 'Fetch any object at any distance', 'Leslie Rollowver');
+// console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+
+import { Component } from '@angular/core';
+
+import { Hero } from './hero';
+
+@Component({
+  selector: 'hero-form',
+  templateUrl: './hero-form.component.html'
+})
+export class HeroFormComponent {
+
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model);
+  }
+}
